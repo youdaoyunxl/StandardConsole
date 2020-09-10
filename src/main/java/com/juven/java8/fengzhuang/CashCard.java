85249a666487e9f52e74ff3510ba75fb81495137
xulei <youdaoyunxl@126.com>
2020-09-10 16:42:26 +0800

    IDEA整合GitHub测试

---
+++ src/main/java/com/juven/java8/fengzhuang/CashCard.java
@@ -0,0 +1,15 @@
+package com.juven.java8.fengzhuang;
+
+public class CashCard {
+
+    String number;
+    int balance;
+    int bonus;
+
+    public CashCard(String number, int balance, int bonus) {
+        this.number = number;
+        this.balance = balance;
+        this.bonus = bonus;
+    }
+
+}
